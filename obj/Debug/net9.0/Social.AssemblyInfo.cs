9598278f939e7e8434a506ce473dee99d56d8993
Leandro Andres Suarez Vece <leovece1@gmail.com>
2025-06-21 14:58:34 -0300

    otro

--- obj/Debug/net9.0/Social.AssemblyInfo.cs
+++ obj/Debug/net9.0/Social.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Social")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d8707e008bc724c84a3774a91d31f6f1d63819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96415f334f493ebc48c8b5ccf2681602219cffee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Social")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Social")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/Services/Auth.js
+++ wwwroot/js/Services/Auth.js
@@ -7,4 +7,4 @@ export const guardarEnLocalStorage = (clave, valor) => {
     localStorage.setItem(clave, JSON.stringify(valor));
 };
 
-export const API_BASE = "http://192.168.0.0/api";
+export const API_BASE = "http://192.168.x.x/api";
